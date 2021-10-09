57f0eb4b89220ca282e921451fff9df7caf227dc
deshani0000 <it19051826@my.sliit.lk>
2021-10-09 11:48:01 +0530

    added edit delivery form

---
+++ machan_eats_application/lib/scr/screens/edit_delivery_details_form.dart
@@ -0,0 +1,449 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:machan_eats_application/validators/delivery_database.dart';
+import 'package:machan_eats_application/validators/validator.dart';
+
+import '../../custom_form_field.dart';
+
+class EditDeliveryDetailsForm extends StatefulWidget {
+
+  final String documentId;
+  final String currentStreet;
+  final String currentCity;
+  final String currentRecieverName;
+  final String currentCountry;
+  final String currentMobileNum;
+
+  final FocusNode streetFocusNode;
+  final FocusNode cityFocusNode;
+  final FocusNode countryFocusNode;
+  final FocusNode recieverNameFocusNode;
+  final FocusNode mobileNumFocusNode;
+
+  const EditDeliveryDetailsForm({
+    required this.documentId,
+    required this.currentStreet,
+    required this.currentCity,
+    required this.currentRecieverName,
+    required this.currentCountry,
+    required this.currentMobileNum,
+    required this.streetFocusNode,
+    required this.cityFocusNode,
+    required this.countryFocusNode,
+    required this.recieverNameFocusNode,
+    required this.mobileNumFocusNode,
+
+  });
+
+  @override
+  _EditDeliveryDetailsFormState createState() => _EditDeliveryDetailsFormState();
+}
+
+class _EditDeliveryDetailsFormState extends State<EditDeliveryDetailsForm> {
+  final _addDeliveryDetailsFormKey = GlobalKey<FormState>();
+
+  bool _isProcessing = false;
+
+  final TextEditingController _streetController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
+  final TextEditingController _recieverNameController = TextEditingController();
+  final TextEditingController _mobileNumController = TextEditingController();
+
+
+  String updateStreet = "";
+  String updateCity = "";
+  String updateCountry = "";
+  String updateRecieverName = "";
+  String updateMobileNum = "";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Form(
+        key: _addDeliveryDetailsFormKey,
+        child: Column(
+          children: <Widget>[
+
+            Padding(
+                padding: const EdgeInsets.only(left:8.0,right: 8.0,bottom: 24.0,),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height:24.0),
+                    Text(
+                      'Delivery Details',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    Divider(
+                      height: 15,
+                    ),
+                    Text(
+                      'Delivery Address',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+
+                    Divider(
+                      height: 5,
+                    ),
+
+
+                    Divider(
+                      height: 25,
+                    ),
+                    Text(
+                      'Street',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 18.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+
+                    Divider(
+                      height: 5,
+                    ),
+
+
+                    CustomFormField(
+
+                      initialValue: widget.currentStreet,
+                      isLabelEnabled: false,
+                      controller: _streetController,
+                      focusNode: widget.streetFocusNode,
+                      keyboardType: TextInputType.text,
+                      inputAction: TextInputAction.next,
+                      validator: (value) {
+                        Validator.validateField(
+                            value: value
+                        );
+                        updateStreet = value;
+                      },
+
+                      label: 'Street',
+                      hint: 'Enter the Street',
+
+                    ),
+
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child: TextField(
+                    //     style: TextStyle(color: Colors.black),
+                    //     decoration: InputDecoration(
+                    //       hintText: "Enter street",
+                    //       border: InputBorder.none,
+                    //       fillColor: Colors.white, filled: true,
+                    //     ),
+                    //
+                    //   ),
+                    // ),
+                    Divider(
+                      height: 25,
+                    ),
+                    Text(
+                      'City',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 18.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+
+                    Divider(
+                      height: 5,
+                    ),
+                    CustomFormField(
+                      initialValue: widget.currentCity,
+                      isLabelEnabled: false,
+                      controller: _cityController,
+                      focusNode: widget.cityFocusNode,
+                      keyboardType: TextInputType.text,
+                      inputAction: TextInputAction.next,
+                      validator: (value) {
+                        Validator.validateField(
+                            value: value
+                        );
+                        updateCity = value;
+                      },
+                      label: 'City',
+                      hint: 'Enter the City',
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child: TextField(
+                    //     style: TextStyle(color: Colors.black),
+                    //     decoration: InputDecoration(
+                    //       hintText: "Enter city",
+                    //       border: InputBorder.none,
+                    //       fillColor: Colors.white, filled: true,
+                    //     ),
+                    //
+                    //   ),
+                    // ),
+                    Divider(
+                      height: 25,
+                    ),
+                    Text(
+                      'Country',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 18.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+
+                    Divider(
+                      height: 5,
+                    ),
+
+                    CustomFormField(
+                      initialValue: widget.currentCountry,
+                      isLabelEnabled: false,
+                      controller: _countryController,
+                      focusNode: widget.countryFocusNode,
+                      keyboardType: TextInputType.text,
+                      inputAction: TextInputAction.next,
+                      validator: (value) {
+                        Validator.validateField(
+                            value: value
+                        );
+                        updateCountry= value;
+                      },
+                      label: 'Country',
+                      hint: 'Enter the Country',
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child: TextField(
+                    //     style: TextStyle(color: Colors.black),
+                    //     decoration: InputDecoration(
+                    //       hintText: "Enter country",
+                    //       border: InputBorder.none,
+                    //       fillColor: Colors.white, filled: true,
+                    //     ),
+                    //
+                    //   ),
+                    // ),
+                    Divider(
+                      height: 25,
+                    ),
+                    Text(
+                      'Receivers Name',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 18.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+
+                    Divider(
+                      height: 5,
+                    ),
+                    CustomFormField(
+                      initialValue: widget.currentRecieverName,
+                      isLabelEnabled: false,
+                      controller: _recieverNameController,
+                      focusNode: widget.recieverNameFocusNode,
+                      keyboardType: TextInputType.text,
+                      inputAction: TextInputAction.next,
+                      validator: (value) {
+                        Validator.validateField(
+                            value: value
+                        );
+                        updateRecieverName = value;
+                      },
+                      label: 'Receiver Name',
+                      hint: 'Enter the Receiver Name',
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child: TextField(
+                    //     style: TextStyle(color: Colors.black),
+                    //     decoration: InputDecoration(
+                    //       hintText: "Enter receivers name",
+                    //       border: InputBorder.none,
+                    //       fillColor: Colors.white, filled: true,
+                    //     ),
+                    //
+                    //   ),
+                    // ),
+                    Divider(
+                      height: 25,
+                    ),
+                    Text(
+                      'Mobile Number',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 18.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+
+                    Divider(
+                      height: 5,
+                    ),
+                    CustomFormField(
+                      initialValue: widget.currentMobileNum,
+                      isLabelEnabled: false,
+                      controller: _mobileNumController,
+                      focusNode: widget.mobileNumFocusNode,
+                      keyboardType: TextInputType.text,
+                      inputAction: TextInputAction.next,
+                      validator: (value) {
+                        Validator.validateField(
+                            value: value
+                        );
+                        updateMobileNum = value;
+                      },
+                      label: 'Mobile Num',
+                      hint: 'Enter the mobile number',
+                    ),
+                  ],
+                )
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: TextField(
+            //     style: TextStyle(color: Colors.black),
+            //     decoration: InputDecoration(
+            //       hintText: "Enter mobile number",
+            //
+            //       border: InputBorder.none,
+            //       fillColor: Colors.white, filled: true,
+            //     ),
+            //
+            //   ),
+            // ),
+
+            Divider(
+              height: 60,
+            ),
+            _isProcessing
+                ? Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+              ),
+            ) :
+            Container(
+              width: double.maxFinite,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.amber),
+                  shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                  ),
+                ),
+                // Padding(
+                //   padding: EdgeInsets.only(left: 40.0, right: 40.0),
+                //   child: Container(
+                //     width: double.maxFinite,
+                //     child: ElevatedButton(
+                //       style: ButtonStyle(
+                //         backgroundColor: MaterialStateProperty.all(Colors.amber),
+                //         shape: MaterialStateProperty.all(
+                //             RoundedRectangleBorder(
+                //               borderRadius: BorderRadius.circular(10),
+                //             )
+                //         ),
+                //       ),
+                onPressed: () async{
+
+                  widget.streetFocusNode.unfocus();
+                  widget.cityFocusNode.unfocus();
+                  widget.countryFocusNode.unfocus();
+                  widget.recieverNameFocusNode.unfocus();
+                  widget.mobileNumFocusNode.unfocus();
+
+
+                  if(_addDeliveryDetailsFormKey.currentState!.validate()){
+                    setState(() {
+                      _isProcessing = true;
+                      // SnackBar(content: Text("Delivery Details Added Successfully"));
+
+                      SnackBar(
+                        content: const Text('Delivery Details Added'),
+
+                      );
+
+                    });
+
+
+                    await Delivery_Database.updateDeliveryDetails(docId: widget.documentId, street: updateStreet, city: updateCity, country: updateCountry, recieverName: updateRecieverName, mobileNum: updateMobileNum);
+
+
+                    setState(() {
+                      _isProcessing = false;
+                    });
+
+                    final snackBar = SnackBar(
+                      content: const Text('Delivery Details Updated!'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    Navigator.of(context).pop();
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(
+                    //     builder: (context) => ShowDetails(),
+                    //   ),
+                    //
+                    //   // DeliveryList(),
+                    // );
+                    // Navigator.of(context).push(
+                    //
+                  }
+
+                },
+
+
+
+
+
+                child: Padding(
+                    padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                    child: Text(
+                        'Update Data',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                          letterSpacing: 2,
+                        )
+                    )
+                ),
+              ),
+            ),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
