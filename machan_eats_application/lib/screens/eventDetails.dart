7498bc1efaa29768e7837dba7e64df949eeb7428
anjanadeseram987 <it19021058@my.sliit.lk>
2021-10-09 12:33:47 +0530

    add events details dart

---
+++ machan_eats_application/lib/screens/eventDetails.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:machan_eats_application/screens/addNewEventScreen.dart';
+import 'package:machan_eats_application/screens/eventList.dart';
+
+import 'addNewEventScreen.dart';
+
+class EventsDetails extends StatefulWidget {
+  const EventsDetails({Key? key}) : super(key: key);
+
+  @override
+  _EventsDetailsState createState() => _EventsDetailsState();
+}
+
+class _EventsDetailsState extends State<EventsDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Padding(
+          padding: EdgeInsets.only(left: 50.0, top: 5.0),
+          child: Text(
+            'Machan Eats',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 25,
+            ),
+          ),
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => AddEventScreen(),
+              ),
+            );
+          },
+          backgroundColor: Colors.amber,
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 32,
+          ),
+        ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0,right: 16.0,bottom: 20,),
+          child: EventList(),
+        ),
+      ),
+    );
+
+  }
+}
+
